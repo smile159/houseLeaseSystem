be4a2d42097e365e11c38c53ac3626652ec9983d
smile159 <1174139226@qq.com>
2022-04-27 14:05:19 +0800

    添加前端和后端

---
+++ src/main/java/com/xinxin/HouseLeaseSystemApplication.java
@@ -0,0 +1,13 @@
+package com.xinxin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HouseLeaseSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HouseLeaseSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/xinxin/HouseLeaseSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xinxin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HouseLeaseSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
